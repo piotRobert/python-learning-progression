398e16bb15dcb21024d683973ae199f1248bef7a
piotRobert <robert.piotrowski@gmail.com>
2025-03-28 19:56:00 +0100

    D-20_21 Part of snake game - snake can be controlled

---
+++ D-20/D-20 snake_game/main.py
@@ -0,0 +1,26 @@
+from turtle import Screen
+from snake import Snake
+import time
+
+screen = Screen()
+screen.setup(width = 600, height = 600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkeypress(snake.up, "Up")
+screen.onkeypress(snake.down, "Down")
+screen.onkeypress(snake.left, "Left")
+screen.onkeypress(snake.right,"Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+    
+screen.exitonclick()
